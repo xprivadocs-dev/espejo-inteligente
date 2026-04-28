77b097733e06e2a81e280ebb20f0745af6229b23
xprivadocs-dev <x.privado.cs@gmail.com>
2026-04-28 17:17:25 -0600

    App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 import React, { useEffect, useRef, useState } from "react";
+import * as faceapi from "face-api.js";
 
 export default function App() {
   const videoRef = useRef();
-  const [estado, setEstado] = useState("Listo");
-  const [mensaje, setMensaje] = useState("Cámara activa");
+  const [estado, setEstado] = useState("Cargando...");
 
   useEffect(() => {
     iniciar();
   }, []);
 
   const iniciar = async () => {
+    // modelos desde internet
+    const URL = "https://justadudewhohacks.github.io/face-api.js/models";
+
+    await faceapi.nets.tinyFaceDetector.loadFromUri(URL);
+    await faceapi.nets.faceExpressionNet.loadFromUri(URL);
+
     const stream = await navigator.mediaDevices.getUserMedia({ video: true });
     videoRef.current.srcObject = stream;
+
+    detectar();
+  };
+
+  const detectar = () => {
+    setInterval(async () => {
+      const detections = await faceapi
+        .detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions())
+        .withFaceExpressions();
+
+      if (detections.length > 0) {
+        const expresiones = detections[0].expressions;
+
+        const max = Object.keys(expresiones).reduce((a, b) =>
+          expresiones[a] > expresiones[b] ? a : b
+        );
+
+        setEstado(max);
+      }
+    }, 3000);
   };
 
   return (
@@ -26,8 +52,7 @@ export default function App() {
         style={{ borderRadius: "10px" }}
       />
 
-      <h2>Estado: {estado}</h2>
-      <p>{mensaje}</p>
+      <h2>Estado detectado: {estado}</h2>
     </div>
   );
 }
